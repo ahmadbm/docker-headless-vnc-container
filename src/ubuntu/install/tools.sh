63c91539a3c0e0bbce347726aed759b63af7ceb1
ahmadbm <ahmad.benmaallem@gmail.com>
2018-05-21 14:58:45 +0200

    Update tools.sh

--- src/ubuntu/install/tools.sh
+++ src/ubuntu/install/tools.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Install some common tools for further installation"
 apt-get update 
-apt-get install -y vim wget net-tools locales gedit bzip2 \
+apt-get install -y vim wget net-tools locales gedit xrdp bzip2 \
     python-numpy #used for websockify/novnc
 apt-get clean -y
 
